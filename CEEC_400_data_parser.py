d197d07faa02a969df0662ef72053fac6b4be0ae
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-12-19 11:42:54 +0200

    Changed recursive loop to write straight to dict in stead of intermediary list.

--- CEEC_400_data_parser.py
+++ CEEC_400_data_parser.py
@@ -2,31 +2,26 @@ import os
 from CEECxml import get_tei_collection_id
 
 
-def list_files_recursive(path: str, xml_path_l: list):
+def find_xml_recursive(path: str, xml_path_d: dict) -> None:
     for entry in os.listdir(path):
         full_path = os.path.join(path, entry)
+
         if os.path.isdir(full_path):
-            list_files_recursive(full_path, xml_path_l)
+            find_xml_recursive(full_path, xml_path_d=xml_path_d)
         elif entry.endswith(".xml"):
-            xml_path_l.append(full_path)
+            teiId = get_tei_collection_id(full_path)
+            xml_path_d[teiId] = full_path
 
-    return xml_path_l
 
+def construct_xml_path_dict(data_path: str) -> dict:
+    tei_xml_path_d = {}
+    find_xml_recursive(path=data_path, xml_path_d=tei_xml_path_d)
 
-def construct_xml_path_dict(data_path: str):
-    l = []
-    xml_paths = list_files_recursive(path=data_path, xml_path_l=l)
-    print(len(xml_paths))
-
-    tei_collection_path_d = {}
-    for path in xml_paths:
-        teiId = get_tei_collection_id(path)
-        tei_collection_path_d[teiId] = path
-    return tei_collection_path_d
+    return tei_xml_path_d
 
 
 if __name__ == "__main__":
-    l = []
     d = construct_xml_path_dict(data_path="./data/CEEC-400/")
-    len(d.keys())
-    # xmls = list_files_recursive(path="./data/CEEC-400/", xml_path_l=l)
+    print(len(d.keys()))
+    # print(d)
+    # xmls = find_xml_recursive(path="./data/CEEC-400/", xml_path_l=l)
